c92a9960f335a6be8366fa3d1875a54d20c3ccf6
wanku-x <wanku@bk.ru>
2018-08-19 03:11:36 +0400

    Добавил новые страницы

--- src/Components/Pages/index.js
+++ src/Components/Pages/index.js
@@ -1,5 +1,7 @@
 import StationPage from './StationPage';
 import NotStationPage from './NotStationPage';
 import NotFound from './NotFound';
+import LoginPage from './LoginPage';
+import AdminPage from './AdminPage';
 
-export { StationPage, NotStationPage, NotFound };
+export { StationPage, NotStationPage, NotFound, LoginPage, AdminPage };

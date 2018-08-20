2a044495a3bf95ea09804cc7a77b50297f718dda
wanku-x <wanku@bk.ru>
2018-08-20 22:33:49 +0400

    Навел красоту в странице 404

--- src/Components/Pages/NotFound.js
+++ src/Components/Pages/NotFound.js
@@ -1,11 +1,27 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { Row, Col, Card, Alert } from 'antd';
 
 class NotFound extends Component {
   render() {
     return (
-      <Fragment>
-        <p>Страница не найдена</p>
-      </Fragment>
+      <Row
+        style={{ minHeight: '100%' }}
+        type="flex"
+        justify="space-between"
+        align="middle"
+      >
+        <Col xs={{ span: 24 }}>
+          <Card className="card">
+            <Alert
+              message="404 Not Found"
+              description="Страница не найдена"
+              type="warning"
+              style={{margin: '24px 0px'}}
+              showIcon
+            />
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }

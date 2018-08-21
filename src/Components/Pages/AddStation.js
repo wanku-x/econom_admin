f1cf6275ba36d14bb284853f905ea1248d5d7b7d
wanku-x <wanku@bk.ru>
2018-08-21 22:35:27 +0400

    К странице добавления станций прикручена API

--- src/Components/Pages/AddStation.js
+++ src/Components/Pages/AddStation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col, Card, Form, Input, InputNumber, Button, message } from 'antd';
 import { requestPOST } from '../Requests';
+import { Loader } from '../Loader';
 
 class AddStation extends Component {
   state = {
@@ -19,6 +20,18 @@ class AddStation extends Component {
     return number;
   }
 
+  normalizeComplexity = (value, prevValue) => {
+    const roundNumber = (Math.round(value * 2) / 2).toFixed(1);
+    if (
+      isNaN(roundNumber) ||
+      (roundNumber < 2) ||
+      (roundNumber > 3)
+    ) {
+      return prevValue;
+    }
+    return roundNumber;
+  }
+
   checkString = (rule, value, callback) => {
     if (value.length < 3) {
       callback('Не менее 3 символов');
@@ -29,20 +42,25 @@ class AddStation extends Component {
     callback();
   }
 
+  checkMaxBet = (rule, max_bet, callback) => {
+    const min_bet = this.props.form.getFieldValue('min_bet');
+    if (min_bet >= max_bet) {
+      callback('Максимальная ставка меньше минимальной');
+    }
+    callback();
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-
     this.props.form.validateFields((err, values) => {
-      console.log(err);
-      console.log(values);
-      /*
       if (!err) {
+        values.complexity = parseFloat(values.complexity);
         this.setState({ loading: true }, () => {
           requestPOST('/api/v1/create_station/', values).then((result)=>{
-            if (result.success) {
+            if (result.status) {
               message.success('Станция успешно создана');
             } else {
-              message.error(result.err);
+              message.error(result.error);
             }
           }).catch((err)=>{
             console.log(err);
@@ -51,7 +69,7 @@ class AddStation extends Component {
             this.setState({ loading: false });
           });
         });
-      }*/
+      }
     })
   }
 
@@ -68,6 +86,10 @@ class AddStation extends Component {
         sm: { span: 12 },
       },
     };
+    const formButtonLayout = {
+      xs: { span: 24 },
+      sm: { span: 18 },
+    }
 
     return (
       <Row
@@ -139,6 +161,7 @@ class AddStation extends Component {
                 {getFieldDecorator('max_bet', {
                   initialValue: 0 ,
                   normalize: this.normalizeBet,
+                  rules: [{ validator: this.checkMaxBet }],
                 })(
                   <Input
                     suffix="$"
@@ -151,9 +174,8 @@ class AddStation extends Component {
                 label="Коэффициент"
               >
                 {getFieldDecorator('complexity', {
-                  initialValue: 2 ,
-                  //normalize: () => {},
-                  //rules: [{ validator: () => {} }],
+                  initialValue: 2.0,
+                  normalize: this.normalizeComplexity,
                 })(
                   <InputNumber
                     size="large"
@@ -164,11 +186,14 @@ class AddStation extends Component {
                 )}
               </FormItem>
               <FormItem>
-                <Button type="primary" htmlType="submit">
-                  Добавить
-                </Button>
+                <Col {...formButtonLayout}>
+                  <Button type="primary" htmlType="submit" style={{ float: 'right' }}>
+                    Добавить
+                  </Button>
+                </Col>
               </FormItem>
             </Form>
+            <Loader isOpen={this.state.loading} />
           </Card>
         </Col>
       </Row>
